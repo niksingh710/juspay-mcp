ff381545b6ee096246b543383def426e32f3d300
Harshpreet Singh <harshpreet.singh@juspay.in>
2025-05-07 23:26:41 +0530

    docs: add license headers to all source files

--- juspay_dashboard_mcp/api/orders.py
+++ juspay_dashboard_mcp/api/orders.py
@@ -1,3 +1,9 @@
+# Copyright 2025 Juspay
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at https://www.apache.org/licenses/LICENSE-2.0.txt
+
 from datetime import datetime, timezone
 from juspay_dashboard_mcp.api.utils import post, get_juspay_host_from_api, call
 from urllib.parse import urlencode
--- juspay_dashboard_mcp/config.py
+++ juspay_dashboard_mcp/config.py
@@ -1,3 +1,9 @@
+# Copyright 2025 Juspay
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at https://www.apache.org/licenses/LICENSE-2.0.txt
+
 import os
 import base64
 import dotenv
